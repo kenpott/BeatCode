3a20ba76543b95130416c6cf1d5ae2334038432d
KuyaKen <ricewitduhspringroll@gmail.com>
2025-10-29 15:58:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 function topKFrequent(nums, k) {
   const freqMap = {};
   const topKFreq = [];
-  let topFreq = 0;
+  let low = 0;
+  let high = 0;
 
+  // this fills the freqMap
   for (let i = 0; i < nums.length; i++) {
     freqMap[nums[i]] = (freqMap[nums[i]] || 0) + 1;
-    if (freqMap[nums[i]] >= topFreq && !topKFreq.includes(nums[i])) {
-      topKFreq.push(nums[i]);
-      topFreq = freqMap[nums[i]];
+
+    if (freqMap[nums[i]] >= low) {
+      if (topKFreq.length > k) {
+        // issue is here
+        topKFreq.shift();
+      }
+      // i have to sort while pushing values into the array (e.g. if statement)
+      if (freqMap[nums[i]] < high) {
+        topKFreq.unshift(nums[i]);
+      } else {
+        // must be greater than the high
+        topKFreq.push(nums[i]);
+      }
     }
-    if (topKFreq.length > k) {
-      topKFreq.shift();
+    // Determining high and low
+    if (low === 0) {
+      low = freqMap[nums[i]];
     }
+    if (freqMap[nums[i]] > high) {
+      high = freqMap[nums[i]];
+    } else if (freqMap[nums[i]] > low) {
+      low = freqMap[nums[i]];
+    }
+    console.log(freqMap, topKFreq, low, high);
   }
-  return topKFreq;
+  return new Set(top);
 }
 
-const nums = [1, 2, 2, 3, 3, 3];
-const k = 2;
+const nums = [3, 0, 1, 0];
+const k = 1;
 
 console.log(topKFrequent(nums, k));
-
-// My solution: assumes that the nums array is in order from least freq to most freq
-
-// track index of the k freq
-// how tf do u not iterate through the map to find out which num has the most freq
-// instead of tracking the topfreq track the minfreq
