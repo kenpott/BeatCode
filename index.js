349e71bcf06cbcaa73dc42362fc73db1dc265136
KuyaKen <92195844+kenpott@users.noreply.github.com>
2025-11-04 11:29:10 -0800

    .

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
-function topKFrequent(nums, k) {
-  const freqMap = {};
-  const topKFreq = [];
-  let low = 0;
-  let high = 0;
+function heapify(arr, n, i) {
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
+  let largest = i;
 
-  // this fills the freqMap
-  for (let i = 0; i < nums.length; i++) {
-    freqMap[nums[i]] = (freqMap[nums[i]] || 0) + 1;
-
-    if (freqMap[nums[i]] >= low&& !topKFreq.includes(nums[i])) {
-      if (topKFreq.length >= k) {
-        topKFreq.shift();
-      }
-      // i have to sort while pushing values into the array (e.g. if statement)
-      if (freqMap[nums[i]] < high) {
-        topKFreq.unshift(nums[i]);
-      } else {
-        topKFreq.push(nums[i]);
-      }
-    }
-    // Determining high and low
-    if (low === 0) {
-      low = freqMap[nums[i]];
-    }
-    if (freqMap[nums[i]] > high) {
-      high = freqMap[nums[i]];
-    } else if (freqMap[nums[i]] > low) {
-      low = freqMap[nums[i]];
-    }
-    console.log(freqMap, topKFreq, low, high);
+  if (left < n && arr[left] > arr[largest]) {
+    largest = left;
   }
-  return topKFreq;
+  if (right < n && arr[right] > arr[largest]) {
+    largest = right;
+  }
+  if (largest !== i) {
+    [arr[i], arr[largest]] = [arr[largest], arr[i]];
+    heapify(arr, n, largest);
+  }
 }
 
-const nums = [3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8, 5, 6];
+function topKFrequent(nums, k) {
+  const startingNode = Math.floor(nums.length / 2) - 1;
+  for (let i = startingNode; i >= 0; i--) {
+    heapify(nums);
+  }
+  return nums;
+}
+
+const nums = [
+  3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8,
+  5, 6,
+];
 const k = 10;
 
 console.log(topKFrequent(nums, k));
 // Expected output: [1,2,5,3,6,7,4,8,10,11]
 
 // the issue is the new low is being too high so it doesnt solve the of unsorted values (low freq to high freq)
+
+// Solution
+// 1. Heapify the frequency map
+// 2. iterate through the heap to find the top k frequent elements
