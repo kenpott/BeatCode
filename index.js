221ab3e31eb146ff5e732b6923f56aae6f15e66c
KuyaKen <92195844+kenpott@users.noreply.github.com>
2025-11-06 19:54:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-function heapify(arr, n, i) {
-  const left = 2 * i + 1;
-  const right = 2 * i + 2;
-  let largest = i;
-
-  if (left < n && arr[left] > arr[largest]) {
-    largest = left;
-  }
-  if (right < n && arr[right] > arr[largest]) {
-    largest = right;
-  }
-  if (largest !== i) {
-    [arr[i], arr[largest]] = [arr[largest], arr[i]];
-    heapify(arr, n, largest);
-  }
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let subString = [];
+        let dupe = 0;
+        let longestSubString = 0;
+        if (s === "") return 0;
+        for (let i = 0; i < 1; i++) {
+            subString.push(s[i]);
+        }
+        longestSubString = subString.length;
+        for (let j = 1; j < s.length; j++) {
+            subString.push(s[j]);
+            let temp = new Set(subString);
+            if (temp.size !== 1) {
+                dupe++
+                if (dupe > k) {
+                    subString.shift();
+                }
+            }
+            longestSubString = Math.max(longestSubString, subString.length);
+        }
+        return longestSubString
+    }
 }
-
-function topKFrequent(nums, k) {
-  const startingNode = Math.floor(nums.length / 2) - 1;
-  for (let i = startingNode; i >= 0; i--) {
-    heapify(nums);
-  }
-  return nums;
-}
-
-const nums = [
-  3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8,
-  5, 6,
-];
-const k = 10;
-
-console.log(topKFrequent(nums, k));
-// Expected output: [1,2,5,3,6,7,4,8,10,11]
-
-// the issue is the new low is being too high so it doesnt solve the of unsorted values (low freq to high freq)
-
-// Solution
-// 1. Heapify the frequency map
-// 2. iterate through the heap to find the top k frequent elements
